09c70fbcfd514e8d9b10983332cf8cea8609d8be
Kashan Ali Abbasi <kashanali456.kr@gmail.com>
2023-05-27 23:51:41 +0500

    Find sfx and music mp3.

---
+++ Programming Theory Project/Assets/Scripts/Manager/MusicManager.cs
@@ -0,0 +1,90 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[DefaultExecutionOrder(-1)]
+public class MusicManager : MonoBehaviour
+{
+    public static MusicManager instance = null;
+
+    void Awake()
+    {
+        if (instance)
+        {
+            Destroy(gameObject);
+        }
+        else
+        {
+            DontDestroyOnLoad(gameObject);
+            instance = this;
+        }
+    }
+
+
+    void Start()
+    {
+        DontDestroyOnLoad(gameObject);
+        CheckSettings();
+
+        GetComponent<AudioSource>().Play();
+
+        //MusicPlay();
+    }
+
+
+    void OnDestroy()
+    {
+
+        if (instance == this)
+        {
+            instance = null;
+        }
+    }
+
+    public void MusicPlay()
+    {
+        GetComponent<AudioSource>().clip = Resources.Load<AudioClip>("SFX/" + "Background Music");
+        GetComponent<AudioSource>().Play();
+    }
+
+    void CheckSettings()
+    {
+        //if (PlayerPrefs.HasKey("Music"))
+        //{
+        //    GetComponent<AudioSource>().volume = PlayerPrefs.GetFloat("Music")/2;
+        //}
+        //else
+        //{
+        //    GetComponent<AudioSource>().volume = 0.3f;
+        //}
+
+        if (PlayerPrefs.HasKey("Music"))
+        {
+            if (PlayerPrefs.GetInt("Music", 1) == 1)
+            {
+                GetComponent<AudioSource>().mute = false;
+            }
+            else
+            {
+                GetComponent<AudioSource>().mute = true;
+            }
+        }
+    }
+
+    public void Mute(bool state, bool save_state, bool save_instantly)
+    {
+        if (GetComponent<AudioSource>() != null)
+        {
+            GetComponent<AudioSource>().mute = state;
+        }
+
+        if (save_state)
+        {
+            int temp_state = state ? 0 : 1;
+            PlayerPrefs.SetInt("Music", temp_state);
+            if (save_instantly)
+            {
+                PlayerPrefs.Save();
+            }
+        }
+    }
+}
---
+++ Programming Theory Project/Assets/Scripts/Manager/SoundManager.cs
@@ -0,0 +1,163 @@
+﻿using UnityEngine;
+
+[DefaultExecutionOrder(-1)]
+public class SoundManager : MonoBehaviour
+{
+    public static SoundManager instance = null;
+
+    [SerializeField] AudioClip audio;
+
+    [SerializeField] bool Debugging = false;
+
+    public bool Allow_Sound_Play = true;
+
+    void Awake()
+    {
+        if (instance)
+        {
+            Destroy(gameObject);
+        }
+        else
+        {
+            DontDestroyOnLoad(gameObject);
+            instance = this;
+        }
+    }
+
+
+    void Start()
+    {
+        DontDestroyOnLoad(gameObject);
+        CheckSettings();
+    }
+
+
+    void OnDestroy()
+    {
+
+        if (instance == this)
+        {
+            instance = null;
+        }
+    }
+
+    public void PlaySound(string AudioName, bool Interupt, float volume)
+    {
+        if (Allow_Sound_Play)
+        {
+            if (Resources.Load<AudioClip>("SFX/" + AudioName) != null)
+            {
+                //GetComponent<AudioSource>().volume = volume;
+                if (!Interupt)
+                {
+                    audio = Resources.Load<AudioClip>("SFX/" + AudioName);
+                    GetComponent<AudioSource>().PlayOneShot(audio, volume);
+                }
+                else
+                {
+                    if (!GetComponent<AudioSource>().isPlaying)
+                    {
+                        audio = Resources.Load<AudioClip>("SFX/" + AudioName);
+                        GetComponent<AudioSource>().PlayOneShot(audio, volume);
+                    }
+                }
+            }
+            else
+            {
+                if (Debugging)
+                {
+                    Debug.LogError("resourse not found");
+                }
+            }
+        }
+    }
+
+    public void PlaySound(AudioSource AudioSource, string AudioName, bool Interupt, float volume)
+    {
+        if (Allow_Sound_Play)
+        {
+            if (Resources.Load<AudioClip>("SFX/" + AudioName) != null && AudioSource != null)
+            {
+                //GetComponent<AudioSource>().volume = volume;
+                if (Interupt)  //old !
+                {
+                    audio = Resources.Load<AudioClip>("SFX/" + AudioName);
+                    AudioSource.PlayOneShot(audio, volume);
+                }
+                else
+                {
+                    if (!AudioSource.isPlaying)
+                    {
+                        audio = Resources.Load<AudioClip>("SFX/" + AudioName);
+                        AudioSource.PlayOneShot(audio, volume);
+                    }
+                }
+            }
+            else
+            {
+                if (Debugging)
+                {
+                    Debug.LogError("resourse not found");
+                }
+            }
+        }
+    }
+
+    public void StopSound(AudioSource AudioSource)
+    {
+        if (AudioSource != null)
+        {
+            AudioSource.Stop();
+        }
+        else
+        {
+            if (Debugging)
+            {
+                Debug.LogError("resourse not found");
+            }
+        }
+    }
+
+
+    void CheckSettings()
+    {
+        //if (PlayerPrefs.HasKey("Sound"))
+        //{
+        //    GetComponent<AudioSource>().volume = PlayerPrefs.GetFloat("Sound");
+        //}
+        //else
+        //{
+        //    GetComponent<AudioSource>().volume = 0.5f;
+        //}
+
+        if (PlayerPrefs.HasKey("Sound"))
+        {
+            if (PlayerPrefs.GetInt("Sound", 1) == 1)
+            {
+                GetComponent<AudioSource>().mute = false;
+            }
+            else
+            {
+                GetComponent<AudioSource>().mute = true;
+            }
+        }
+    }
+
+    public void Mute(bool state, bool save_state, bool save_instantly)
+    {
+        if (GetComponent<AudioSource>() != null)
+        {
+            GetComponent<AudioSource>().mute = state;
+        }
+
+        if (save_state)
+        {
+            int temp_state = state ? 0 : 1;
+            PlayerPrefs.SetInt("Sound", temp_state);
+            if (save_instantly)
+            {
+                PlayerPrefs.Save();
+            }
+        }
+    }
+}
